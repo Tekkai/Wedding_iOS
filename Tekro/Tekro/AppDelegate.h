f74fdfa4b84ef542c85c4dd473c8ce0eef515957
Tekkai <pmoreno@iMac-de-Rocio-2.local>
2014-12-23 13:18:28 +0100

    init

---
+++ Tekro/Tekro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tekro
+//
+//  Created by Pau Moreno on 23/12/14.
+//  Copyright (c) 2014 Tekro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tekro/Tekro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tekro
+//
+//  Created by Pau Moreno on 23/12/14.
+//  Copyright (c) 2014 Tekro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
